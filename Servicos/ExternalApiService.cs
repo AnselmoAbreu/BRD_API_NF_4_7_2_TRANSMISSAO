c4e9d4b3ab945d690be0d65bb1bdfd06cc59f580
AnselmoAbreu <anselmo.mda@outlook.com>
2025-03-09 18:20:47 -0300

    commit de teste

--- Servicos/ExternalApiService.cs
+++ Servicos/ExternalApiService.cs
@@ -5,24 +5,24 @@ namespace BRD_API_NF_4_7_2_TRANSMISSAO.Servicos
 {
 	public class ExternalApiService
 	{
-        //private static readonly HttpClient client = new HttpClient();
+		//private static readonly HttpClient client = new HttpClient();
 
-        public async Task<string> CallExternalApiAsync(string apiUrl)
-        {
-            var handler = new HttpClientHandler();
-            handler.ServerCertificateCustomValidationCallback = (message, cert, chain, errors) => true;
-            HttpClient client = new HttpClient();
-            try
-            {
-                HttpResponseMessage response = await client.GetAsync(apiUrl);
-                response.EnsureSuccessStatusCode();
-                string responseBody = await response.Content.ReadAsStringAsync();
-                return responseBody;
-            }
-            catch (HttpRequestException e)
-            {
-                return $"Request error: {e.Message}";
-            }
-        }
-    }
+		public async Task<string> CallExternalApiAsync(string apiUrl)
+		{
+			var handler = new HttpClientHandler();
+			handler.ServerCertificateCustomValidationCallback = (message, cert, chain, errors) => true;
+			HttpClient client = new HttpClient();
+			try
+			{
+				HttpResponseMessage response = await client.GetAsync(apiUrl);
+				response.EnsureSuccessStatusCode();
+				string responseBody = await response.Content.ReadAsStringAsync();
+				return responseBody;
+			}
+			catch (HttpRequestException e)
+			{
+				return $"Request error: {e.Message}";
+			}
+		}
+	}
 }
